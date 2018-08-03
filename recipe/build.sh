b110d2ab02f3a854f8c76de637d14f9047d3f17d
santi <sdvillal@gmail.com>
2018-08-03 18:25:52 +0200

    bump version, add run_exports, use CF pins
    - removes unsafe libidn
    - adapts pkg-config patch
    - removes makefile patch, that does not seem needed anymore
    - makes linter happy with github changed name

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-autoreconf -fi
-./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit
+
+# disable libidn for security reasons:
+#   http://lists.gnupg.org/pipermail/gnutls-devel/2015-May/007582.html
+# if ever want it back, package and link against libidn2 instead
+
+./configure --prefix="${PREFIX}" \
+            --without-idn \
+            --without-libidn2 \
+            --with-included-libtasn1 \
+            --with-included-unistring \
+            --without-p11-kit
 make
 make install
 make check
