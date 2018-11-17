ec1194e01a8b1c87dfd2ef1458114e1146e01183
Isuru Fernando <isuruf@gmail.com>
2018-11-17 00:49:28 -0600

    Add -rpath-link

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,17 @@
 #   http://lists.gnupg.org/pipermail/gnutls-devel/2015-May/007582.html
 # if ever want it back, package and link against libidn2 instead
 
+if [ "$(uname)" == "Linux" ]
+then
+   export LDFLAGS="$LDFLAGS -Wl,-rpath-link,${PREFIX}/lib"
+fi
+
 ./configure --prefix="${PREFIX}" \
             --without-idn \
             --without-libidn2 \
             --with-included-libtasn1 \
             --with-included-unistring \
             --without-p11-kit
-make
+make -j${CPU_COUNT}
 make install
 make check
