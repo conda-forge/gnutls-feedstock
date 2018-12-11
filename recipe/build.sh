1a7a44f4899726d46625768845b4abe8b8794831
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-12-10 22:08:17 -0500

    Updated to a new version and moved nettle pin

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,14 @@ fi
 #   http://lists.gnupg.org/pipermail/gnutls-devel/2015-May/007582.html
 # if ever want it back, package and link against libidn2 instead
 
+export CPPFLAGS="${CPPFLAGS//-DNDEBUG/}"
+
 ./configure --prefix="${PREFIX}" \
             --without-idn \
             --without-libidn2 \
             --with-included-libtasn1 \
             --with-included-unistring \
-            --without-p11-kit
+            --without-p11-kit || { cat config.log; exit 1; }
 make -j${CPU_COUNT}
 make install
-make -j${CPU_COUNT} check V=1
+make -j${CPU_COUNT} check V=1 || { cat tests/test-suite.log; exit 1; }
