f93e9efc855aa42bfe15fd019c8fcb93fb398a76
Anthony Scopatz <scopatz@gmail.com>
2018-12-13 16:54:05 -0500

    disabling slow tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,16 @@ fi
 # disable libidn for security reasons:
 #   http://lists.gnupg.org/pipermail/gnutls-devel/2015-May/007582.html
 # if ever want it back, package and link against libidn2 instead
+#
+# Also --disable-full-test-suite does not disable all tests but rather
+# "disable[s] running very slow components of test suite"
 
 export CPPFLAGS="${CPPFLAGS//-DNDEBUG/}"
 
 ./configure --prefix="${PREFIX}" \
             --without-idn \
             --without-libidn2 \
+            --disable-full-test-suite \
             --with-included-libtasn1 \
             --with-included-unistring \
             --without-p11-kit || { cat config.log; exit 1; }
