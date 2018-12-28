eef81e61975d635a9f55ffca8407102342404a73
Ray Donnelly <mingw.android@gmail.com>
2018-12-27 20:50:58 -0600

    libtoolize for as-needed

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,14 +13,21 @@ fi
 
 export CPPFLAGS="${CPPFLAGS//-DNDEBUG/}"
 
-./configure --prefix="${PREFIX}" \
-            --without-idn \
-            --cache-file=test-output.log \
-            --disable-full-test-suite \
-            --disable-maintainer-mode \
-            --with-included-libtasn1 \
-            --with-included-unistring \
+libtoolize --copy --force --verbose
+# libtoolize deletes things we need from build-aux, this puts them back
+automake --add-missing --copy --verbose
+
+./configure --prefix="${PREFIX}"          \
+            --without-idn                 \
+            --cache-file=test-output.log  \
+            --disable-full-test-suite     \
+            --disable-maintainer-mode     \
+            --with-included-libtasn1      \
+            --with-included-unistring     \
             --without-p11-kit || { cat config.log; exit 1; }
-make -j${CPU_COUNT}
+
+cat libtool | grep as-needed 2>&1 >/dev/null || { echo "ERROR: Not using libtool with --as-needed fixes?"; exit 1; }
+
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
 make -j${CPU_COUNT} check V=1 || { echo CONDA-FORGE TEST OUTPUT; cat test-output.log; cat tests/test-suite.log; cat tests/slow/test-suite.log; exit 1; }
