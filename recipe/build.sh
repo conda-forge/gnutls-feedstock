dd5b89d332931cb052c6aa43d2312ce385204222
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-03-17 14:44:48 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,7 +31,7 @@ cat libtool | grep as-needed 2>&1 >/dev/null || { echo "ERROR: Not using libtool
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
-if [[ "${target_platform}" == "linux-ppc64le" ]; then
+if [[ "${target_platform}" == "linux-ppc64le" ]]; then
    export fail_test_exit_code="0"
 else
    export fail_test_exit_code="1"
